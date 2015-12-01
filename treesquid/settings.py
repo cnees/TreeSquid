a7aaa2cddf7a927cd82d8211823cbb40c361b305
chrisloechli <chrisloechli@gmail.com>
2015-12-01 12:51:15 -0500

    Testing settings on Heroku

--- treesquid/settings.py
+++ treesquid/settings.py
@@ -83,17 +83,7 @@ DATABASES = { 'default': dj_database_url.config() }
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'treesquid',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'treesquid_user',
-        'PASSWORD': 'treesquid',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
+
 
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
